d87cb45be96d0fbd46519cf5e97278c778e63df4
Saranyaullas1999 <saranyaullas1999@gmail.com>
2022-10-11 18:06:46 +0530

    api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,7 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { AirlinesComponent } from './airlines/airlines.component';
 import { RouterModule ,Routes} from '@angular/router';
 import { PassangersComponent } from './passangers/passangers.component';
-
+import {HttpClientModule} from '@angular/common/http'
 
 const appRoutes:Routes=[
   {
@@ -27,7 +27,8 @@ const appRoutes:Routes=[
   imports: [
     BrowserModule,
     AppRoutingModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]

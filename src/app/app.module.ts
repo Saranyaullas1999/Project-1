8cbf381bd36868a965f756f0838eb44ecde040df
Saranyaullas1999 <saranyaullas1999@gmail.com>
2022-10-11 17:09:27 +0530

    routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { AirlinesComponent } from './airlines/airlines.component';
+import { RouterModule ,Routes} from '@angular/router';
 
+
+const appRoutes:Routes=[
+  {
+    path:"",component:AirlinesComponent
+  }
+]
 @NgModule({
   declarations: [
     AppComponent,
@@ -14,7 +21,8 @@ import { AirlinesComponent } from './airlines/airlines.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
